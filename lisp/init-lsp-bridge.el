35acc569b4daf3182492b35dbd320e370d0d05c6
Joe Chen <geometryolife@gmail.com>
2023-05-01 14:30:42 +0800

    canonicalize the location of lsp-bridge

--- lisp/init-lsp-bridge.el
+++ lisp/init-lsp-bridge.el
@@ -1,5 +1,5 @@
 ;;  A blazingly fast LSP client for Emacs
-(add-to-list 'load-path "~/i/lsp-bridge")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/elpa/lsp-bridge"))
 
 (require 'lsp-bridge)
 (global-lsp-bridge-mode)
