48d6252dbaf89091dd170f9373c46d1c5cd1d7cd
Joe Chen <geometryolife@gmail.com>
2023-05-01 11:59:44 +0800

    add (M-m s) and (M-m c) to translate (C-c C-s) and (C-c C-c)

--- lisp/init-markdown-mode.el
+++ lisp/init-markdown-mode.el
@@ -2,4 +2,10 @@
 
 (setq markdown-asymmetric-header t)
 
+;; Translate the markdown keymaps only when markdown-mode is activated
+(add-hook 'markdown-mode-hook
+		  (lambda ()
+			(define-key key-translation-map (kbd "M-m s") (kbd "C-c C-s"))
+			(define-key key-translation-map (kbd "M-m c") (kbd "C-c C-c"))))
+
 (provide 'init-markdown-mode)
