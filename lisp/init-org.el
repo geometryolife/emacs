5f8fca2f839eca86a9529bd97fd04851ea187d7f
Joe Chen <geometryolife@gmail.com>
2023-05-07 00:59:14 +0800

    add ox-hugo

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -28,4 +28,9 @@
 
 (global-set-key (kbd "C-c r") 'org-capture)
 
+(use-package ox-hugo
+  :ensure t   ;Auto-install the package from Melpa
+  :pin melpa  ;`package-archives' should already have ("melpa" . "https://melpa.org/packages/")
+  :after ox)
+
 (provide 'init-org)
